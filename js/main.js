ceb3f9100d42d3c6505cd43d48c7755fbbb7a7e1
Luismoteando <LuisMiguel.Ortiz.Rozalen@alumnos.upm.es>
2019-11-10 11:24:41 +0100

    Nav design enhancements

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
+$(document).ready(() => {
+    $(window).scroll(() => {
         if ($(this).scrollTop() > 56) {
-            $('#indexNavbar').removeClass('bg-transparent').addClass('bg-dark shadow');
+            $("#navIndex").removeClass("bg-transparent").addClass("bg-dark shadow");
         } else {
-            $('#indexNavbar').removeClass('bg-dark shadow').addClass('bg-transparent');
+            $("#navIndex").removeClass("bg-dark shadow").addClass("bg-transparent");
+        }
+    });
+    document.getElementById("btnToggler").addEventListener("click", function () {
+        if ($(window).scrollTop() > 56) {
+            if ($("#navIndex").hasClass("bg-transparent")) {
+                $("#navIndex").removeClass("bg-transparent").addClass("bg-dark shadow");
+            }
+        } else {
+            if ($("#navIndex").hasClass("bg-transparent")) {
+                $("#navIndex").removeClass("bg-transparent").addClass("bg-dark shadow");
+            } else {
+                $("#navIndex").removeClass("bg-dark shadow").addClass("bg-transparent");
+            }
         }
     });
 });
